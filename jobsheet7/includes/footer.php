c02b9790b673d6e7be4e204adefa4759f3a93321
nailaamanda883 <nailaamanda883@gmail.com>
2026-09-18 13:58:55 +0700

    jb7

---
+++ jobsheet7/includes/footer.php
@@ -0,0 +1,16 @@
+<?php
+if (!isset($base)) {
+    $base = '';
+}
+?>
+
+</main>
+
+<footer>
+    <p>&copy; 2026 SIMPUS-Mini &mdash; Jobsheet 7</p>
+</footer>
+
+<script src="<?php echo $base; ?>assets/js/app.js"></script>
+
+</body>
+</html>
